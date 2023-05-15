52f064fc7be5cf1fbf07206918206894d3f90f8e
Victor Melo <contatomelo2@gmail.com>
2023-05-14 23:24:52 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,18 @@
 const botaoAlterarTema = document.getElementById("botao-alterar-tema");
-
 const body = document.querySelector("body")
-
 const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao")
 
 botaoAlterarTema.addEventListener("click", () => {
-    body.classList.add("modo-escuro");
-
-    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png")
-
     const modoEscuroEstaAtivo = body.classList.contains("modo-escuro")
+    if (modoEscuroEstaAtivo) {
+        body.classList.remove("modo-escuro");
 
-    if(modoEscuroEstaAtivo) {
-        
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
+
+    } else {
+        body.classList.add("modo-escuro");
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png")
     }
-
-
 });
 
 
